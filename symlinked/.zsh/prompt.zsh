ff89b8368b8d4136534fd2a6beba4fe91c80ee7a
hertogp <git.hertogp@github.com>
2015-12-26 23:42:49 +0100

    prompt.zsh

--- symlinked/.zsh/prompt.zsh
+++ symlinked/.zsh/prompt.zsh
@@ -29,56 +29,47 @@ GIT_PROMPT_MERGING="%{$fg_bold[magenta]%}⚡︎%{$reset_color%}"
 GIT_PROMPT_UNTRACKED="%{$fg_bold[red]%}u%{$reset_color%}"
 GIT_PROMPT_MODIFIED="%{$fg_bold[yellow]%}m%{$reset_color%}"
 GIT_PROMPT_STAGED="%{$fg_bold[green]%}s%{$reset_color%}"
- 
+
 # Show Git branch/tag, or name-rev if on detached head
 function parse_git_branch() {
   (git symbolic-ref -q HEAD || git name-rev --name-only --no-undefined --always HEAD) 2> /dev/null
 }
- 
+
 # Show different symbols as appropriate for various Git repository states
 function parse_git_state() {
- 
   # Compose this value via multiple conditional appends.
   local GIT_STATE=""
- 
   local NUM_AHEAD="$(git log --oneline @{u}.. 2> /dev/null | wc -l | tr -d ' ')"
   if [ "$NUM_AHEAD" -gt 0 ]; then
     GIT_STATE=$GIT_STATE${GIT_PROMPT_AHEAD//NUM/$NUM_AHEAD}
   fi
- 
   local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
   if [ "$NUM_BEHIND" -gt 0 ]; then
     GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
   fi
- 
   local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
   if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
     GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
   fi
- 
   if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
     GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
   fi
- 
   if ! git diff --quiet 2> /dev/null; then
     GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
   fi
- 
   if ! git diff --cached --quiet 2> /dev/null; then
     GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
   fi
- 
   if [[ -n $GIT_STATE ]]; then
     echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"
   fi
- 
 }
- 
 
 # If inside a Git repository, print its branch and state
 function git_prompt_string() {
   local git_where="$(parse_git_branch)"
-  [ -n "$git_where" ] && echo " %{$fg[red]%}(${git_where#(refs/heads/|tags/)})$(parse_git_state)"
+  #[ -n "$git_where" ] && echo " %{$fg[red]%}(${git_where#(refs/heads/|tags/)})$(parse_git_state)%{reset_color}"
+  [ -n "$git_where" ] && echo " %{$fg[red]%}(${git_where#(refs/heads/|tags/)})%{$reset_color%}$(parse_git_state)"
 }
 
 # determine Ruby version whether using RVM or rbenv
@@ -98,11 +89,14 @@ chpwd_functions+=(_update_ruby_version)
 function current_pwd {
   echo $(pwd | sed -e "s,^$HOME,~,")
 }
+function cur_git_root {
+  echo ${$(git rev-parse --show-toplevel)/*\/}
+}
 
 PROMPT='
-${PR_GREEN}%n%{$reset_color%}%{$FG[239]%}@%{$reset_color%}${PR_BOLD_BLUE}$(box_name)%{$reset_color%}%{$FG[239]%}:%{$reset_color%}${PR_BOLD_YELLOW}$(current_pwd)%{$reset_color%}$(git_prompt_string)
-$(prompt_char) '
+${PR_GREEN}%n%{$reset_color%}${PR_YELLOW}@%{$reset_color%}${PR_GREEN}$(box_name)%{$reset_color%}%{$FG[239]%}:%{$reset_color%} ${PR_BOLD_YELLOW}$(cur_git_root)%{$reset_color%}$(git_prompt_string)$(prompt_char) '
 
+# ${PR_GREEN}%n%{$reset_color%}%{$FG[239]%}@%{$reset_color%}${PR_GREEN}$(box_name)%{$reset_color%}%{$FG[239]%}:%{$reset_color%}${PR_BOLD_YELLOW}$(current_pwd)%{$reset_color%}$(git_prompt_string)
 export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [(y)es (n)o (a)bort (e)dit]? "
 
 RPROMPT='${PR_GREEN}$(virtualenv_info)%{$reset_color%} ${PR_RED}${ruby_version}%{$reset_color%}'
