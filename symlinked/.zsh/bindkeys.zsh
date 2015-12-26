6c014498bdfc7194fbc059afe9b70d8e8e04b5fd
hertogp <git.hertogp@github.com>
2015-12-26 22:13:45 +0100

    mapped caps to control

--- symlinked/.zsh/bindkeys.zsh
+++ symlinked/.zsh/bindkeys.zsh
@@ -13,3 +13,16 @@ bindkey "^F"      forward-char                         # ctrl-f
 bindkey "^B"      backward-char                        # ctrl-b
 bindkey -v   # Default to standard vi bindings, regardless of editor string
 
+# map caps to control
+# - alternative: setxkbmap -option 'ctrl:nocaps'
+# xmodmap ~/.xmodmaprc
+# - show current keyboard options: setxkbmap -query
+setxkbmap -option 'ctrl:nocaps'
+
+# Use US keyboard layout, variant ALTGR-INTL
+# - right-alt key to get éÉ-characters².
+# - not sure if bashrc is the best place to do this
+# - see https://wiki.archlinux.org/index.php/Keyboard_configuration_in_Xorg
+# - couldn't find anything in /etc/X11
+# - see /etc/default/keyboard
+setxkbmap -rules evdev -model evdev -layout us -variant altgr-intl
