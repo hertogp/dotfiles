24a2f82bc6abdb66a9d9293bd4c520348d63ec07
hertogp <git.hertogp@github.com>
2015-12-29 11:53:47 +0100

    prompt traffic lights

--- symlinked/.zsh/prompt.zsh
+++ symlinked/.zsh/prompt.zsh
@@ -1,27 +1,25 @@
-# See also http://zanshin.net/2013/02/02/zsh-configuration-from-the-ground-up
-# - rewrote git prompt a little bit, make it simpler
+# See also:
+# - http://zanshin.net/2013/02/02/zsh-configuration-from-the-ground-up
+# - http://blog.joshdick.net/2012/12/30/my_git_prompt_for_zsh.html
+# - https://gist.github.com/4415470
+# - https://gist.github.com/1712320
+
 # Functions {{{1
-# determine Ruby version whether using RVM or rbenv
-# the chpwd_functions line cause this to update only when the directory changes
 function _update_ruby_version() { #{{{2
-    typeset -g ruby_version=''
-    if which rvm-prompt &> /dev/null; then
-      ruby_version="$(rvm-prompt i v g)"
-    else
-      if which rbenv &> /dev/null; then
-        ruby_version="$(rbenv version | sed -e "s/ (set.*$//")"
-      fi
-    fi
+  # Get active ruby version using rbenv or rvm
+  typeset -g ruby_version=''
+  which rbenv 1>/dev/null 2>/dev/null && ruby_version=${${(A)="$(rbenv version)"}[1]:l} && return
+  which rvm 1>/dev/null 2>/dev/null && ruby_version="$(rvm i v g)" && return
+  ruby_version="<?>"
 }
+
+# Only update global ruby_version when changing directories
 chpwd_functions+=(_update_ruby_version)
 
 function addon_gitty(){  #{{{2
   # (arrays hashes) declared inside funcs are local by nature
   local GIT_ROOT=$(git rev-parse --show-toplevel 2>/dev/null)
   [[ -z $GIT_ROOT ]] && return
-
-  local msg=""
-  msg="%{$fg_bold[yellow]%}$(basename $GIT_ROOT)%{$reset_color%}"
 
   typeset -A field
   field=(branch '' ahead '' behind '')
@@ -48,39 +46,37 @@ function addon_gitty(){  #{{{2
 
   # if we don't have a branch, report nothing
   [[ -z ${field[branch]} ]] && return
-  # cumulative build of state flags
+
+  # First, build traffic lights based on flags
   local state=""
   [[ -n ${field[ahead]} ]]  && state="$state%{$fg[cyan]%}+${field[ahead]}"
   [[ -n ${field[behind]} ]] && state="$state%{$fg[cyan]%}-${field[behind]}"
-  ${field[modified]}  && state="${state}%{$fg[red]%}m"
-  ${field[staged]}    && state="${state}%{$fg[green]%}s"
-  ${field[untracked]} && state="${state}%{$fg[yellow]%}u"
-  ${field[ignored]}   && state="${state}%{$fg[cyan]%}i"
+  ${field[modified]}  && state="${state}%{$fg_bold[red]%}●%{$reset_color%}"
+  ${field[staged]}    && state="${state}%{$fg_bold[green]%}●%{$reset_color%}"
+  ${field[untracked]} && state="${state}%{$fg_bold[magenta]%}●%{$reset_color%}"
+  ${field[ignored]}   && state="${state}%{$fg_bold[cyan]%}●%{$reset_color%}"
   ${field[conflict]}  && state="${state}%{$fg_bold[red]%}⚡︎%{$reset_color%}"
-  [[ -n $state ]]     && state=" $state"
 
-  msg+=" %{$fg[red]%}(${field[branch]}$state%{$fg[red]%})"
-  [[ -z $state ]] && msg="${msg}%{$fg_bold[yellow]%} ✓ %{$reset_color%}"
-  [[ -n $state ]] && msg="${msg}%{$fg_bold[red]%} ✗ %{$reset_color%}"
-  echo $msg
+  local msg=""
+  msg="%{$fg_bold[yellow]%}$(basename $GIT_ROOT)%{$reset_color%}"
+  msg+=" %{$fg[red]%}(${field[branch]}%)[$state%{$fg[red]%}]%{$reset_color%}"
+  [[ -z $state ]] && msg="$msg %{$fg_bold[yellow]%}✓ %{$reset_color%}"
+  [[ -n $state ]] && msg="$msg %{$fg_bold[red]%}✗ %{$reset_color%}"
+  echo "$msg← "
 }
+
 #setopt promptsubst
 autoload -U colors && colors # Enable colors in prompt
-# PROMPT(s) {{{1
-# http://blog.joshdick.net/2012/12/30/my_git_prompt_for_zsh.html
-# copied from https://gist.github.com/4415470
-# Adapted from code found at <https://gist.github.com/1712320>.
 
-# Get_gitty
-# Create git status flags from $(git status -bsu --porcelain)
+# PROMPT(s) {{{1
 
 # left-prompt {{{2
-# echo "%{$fg[green]%}%n%{$reset_color%}@%{$fg[green]%}%m%{$reset_color%}"
 PROMPT='
-%{$fg[green]%}%n%{$reset_color%}@%{$fg[green]%}%m%{$reset_color%}%# $(addon_gitty)'
+%{$fg[green]%}%n%{$reset_color%}@%{$fg[green]%}%m%{$reset_color%} → $(addon_gitty)%~
+%# '
 
+# sprompt {{{2
 export SPROMPT="Correct $fg[red]%R$reset_color to $fg[green]%r$reset_color [(y)es (n)o (a)bort (e)dit]? "
 
 # right-prompt {{{2
-RPROMPT='${PR_RED}${ruby_version}%{$reset_color%}'
-
+RPROMPT='%{$fg[red]%}[${ruby_version}]%{$reset_color%}'
