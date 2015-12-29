2fc0c3f30dc421e7a01fdba35ef904fee51993dd
hertogp <git.hertogp@github.com>
2015-12-29 13:05:11 +0100

    LS_COLORS, changed colors for ln, di, and ex

--- symlinked/.zsh/colors.zsh
+++ symlinked/.zsh/colors.zsh
@@ -18,4 +18,17 @@ export CLICOLOR=1
 # Main change, you can see directories on a dark background
 #export LS_COLORS=gxfxcxdxbxegedabagacad
 #export LS_COLORS=exfxcxdxbxegedabagacad
-export LS_COLORS="di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=01;32";
+# See also:
+# - http://www.bigsoft.co.uk/blog/index.php/2008/04/11/configuring-ls_colors
+# - LS_COLORS=<key=colorcode;colorcode;..>:<key=effect>: ..
+# - keys include:
+#   no normal, fi file, di dir, ln link, st sticky, ex executable ... etc
+# - color codes:
+#   effects:    00 default, 01 bold, 04 underline, 05 flash, 07 reversed, 08
+#   fg-colors : 30 black, 31 red, 32 green, 33 orange, 34 blue, 35 purple, 36 cyan, 37 grey
+#   bg-colors : 40 black, 41 red, 42 green, 43 orange, 44 blue, 45 purple, 46 cyan, 47 grey
+#   extra fg-col: 90 darkgrey, 91 lightred, 92 lightgreen, 93 yellow, 94 lightblue, 95 light purple
+#                 96 turquoise, 97 white
+#   extra bg-col: bg-color+10 (except for white), so 102 is lighgreen background
+#
+export LS_COLORS="di=00;94:ln=00;95:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=00;32";
