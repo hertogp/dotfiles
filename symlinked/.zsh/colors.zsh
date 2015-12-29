6022ce3b0544de6ad481f50a327eef753688391e
hertogp <git.hertogp@github.com>
2015-12-29 12:22:37 +0100

    fixed LS_COLORS, I think ..

--- symlinked/.zsh/colors.zsh
+++ symlinked/.zsh/colors.zsh
@@ -14,9 +14,8 @@ export PR_BOLD_WHITE PR_BOLD_BLACK
 
 # Clear LS_COLORS
 unset LS_COLORS
-
+export CLICOLOR=1
 # Main change, you can see directories on a dark background
 #export LS_COLORS=gxfxcxdxbxegedabagacad
-
-export CLICOLOR=1
-export LS_COLORS=exfxcxdxbxegedabagacad
+#export LS_COLORS=exfxcxdxbxegedabagacad
+export LS_COLORS="di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:su=37;41:sg=30;43:tw=30;42:ow=34;42:st=37;44:ex=01;32";
