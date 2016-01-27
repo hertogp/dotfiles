199f69fbeccfb11d723d6359b44337a2d198f490
hertogp <git.hertogp@github.com>
2016-01-27 09:58:13 +0100

    no shared_history

--- symlinked/.zsh/checks.zsh
+++ symlinked/.zsh/checks.zsh
@@ -1,19 +1,6 @@
-if [[ $(uname) = 'Linux' ]]; then
-    IS_LINUX=1
-fi
-
-if [[ $(uname) = 'Darwin' ]]; then
-    IS_MAC=1
-fi
-
-if [[ -x `which brew` ]]; then
-    HAS_BREW=1
-fi
-
-if [[ -x `which apt-get` ]]; then
-    HAS_APT=1
-fi
-
-if [[ -x `which yum` ]]; then
-    HAS_YUM=1
-fi
+# Perform some checks & set corresponding flags.
+[[ $(uname) = Linux ]] && IS_LINUX=1
+[[ $(uname) = Darwin ]] && IS_MAC=1
+[[ -x `which brew` ]] && HAS_BREW=1
+[[ -x `which apt-get` ]] && HAS_APT=1
+[[ -x `which yum` ]] && HAS_YUM=1
